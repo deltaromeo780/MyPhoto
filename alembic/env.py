aa7a968a8cd98b8a7954abe22f67daca9048bfbe
Paul <65183959+Pawel780@users.noreply.github.com>
2025-01-28 01:07:08 +0100

    swagger user

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,8 @@
 from logging.config import fileConfig
-
+from app.db import Base
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from app.db import Base, engine
-from app.models import Photo
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -19,8 +18,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata  # Określamy, które modele będą używane w migracjach
-
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
